7b4f2e786d3a53e9427933874ac9df5e8e9fe11d
root <root@dair-boosterpack.localdomain>
2020-10-19 10:28:40 -0400

    working postgres

--- config/settings.py
+++ config/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'acid-minimal-cluster.default.svc.cluster.local',
+        'PASSWORD': 'iXCMR7t2WXLAs0ildLNp3DsEexgpFtBQUxJJVYNtWi5QEFmcJHkpuYJSlEHiic2Z',
+        'PORT': '5432'
     }
 }
 
